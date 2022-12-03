91d1d076b5f5fbaa6b16ab2b1bd17a130fb15616
Yavor Shahpasov <yavosh@gmail.com>
2022-12-03 21:58:34 +0200

    day1 a bit rusty

---
+++ src/bin/01.rs
@@ -0,0 +1,52 @@
+fn sum(input: &str) -> Result<Vec<u32>, Box<dyn std::error::Error>> {
+    let mut elves: Vec<u32> = vec![];
+    let mut sum: u32 = 0;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            elves.push(sum);
+            sum = 0;
+            continue;
+        }
+
+        let cals: u32 = line.trim().parse()?;
+        sum += cals;
+    }
+
+    return Ok(elves)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let elves = sum(input).unwrap();
+    Some(*elves.iter().max().unwrap() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut elves = sum(input).unwrap();
+    elves.sort();
+
+    Some(elves[elves.len() - 3..elves.len()].iter().sum())
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
